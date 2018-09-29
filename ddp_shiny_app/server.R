a69564d983c755c9008a2227f587543d6ce070fe
msinjin <mark@msjsci.com>
2018-09-29 17:56:00 -0400

    Initial commit

---
+++ ddp_shiny_app/server.R
@@ -0,0 +1,44 @@
+library(shiny)
+
+shinyServer(
+    function(input, output, session) {
+       
+        set.seed(42)
+        library(caret)
+ 
+        model_list <- reactive({
+            
+            withProgress({
+                
+                myFolds <- createFolds(iris$Species, 
+                                       k = input$num_folds, 
+                                       list = T, 
+                                       returnTrain = T) 
+                trControl <- trainControl(method = "repeatedcv", 
+                                          classProbs = T, 
+                                          repeats = input$num_reps, 
+                                          index = myFolds, 
+                                          verboseIter = F, 
+                                          savePredictions = T)
+                
+                library(caretEnsemble)
+                library(ranger) # alternative to base rf model
+                model_list <- caretList(Species ~ .,
+                                        data = iris,
+                                        methodList = c(lda = "lda", 
+                                                       gbm = "gbm", 
+                                                       rf = "ranger"),
+                                        trControl = trControl,
+                                        tuneLength = 3)
+                
+            }, 
+            message = 'Running model...', 
+            value = 0.5)
+            })
+       
+        output$plot1 <- renderPlot({
+        resamps <- resamples(model_list())
+         dotplot(resamps)
+        })
+    }
+    )
---
+++ ddp_shiny_app/ui.R
@@ -0,0 +1,42 @@
+library(shiny)
+shinyUI(fluidPage(
+    headerPanel("Modeling Flowers"),
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput(
+                'num_folds',
+                'Number of cross validation folds',
+                min = 2,
+                max = 10,
+                value = 5,
+                step = 1,
+                dragRange = F
+            ),
+            sliderInput(
+                'num_reps',
+                'Number of cross validation repeats',
+                min = 1,
+                max = 10,
+                value = 3,
+                step = 1,
+                dragRange = F
+            )
+        ),
+        mainPanel(# h3("Confusion matrix for your model"),
+            # tableOutput('confusion')
+            # textOutput("error_rate"),
+            plotOutput('plot1'))
+    ),
+    wellPanel(
+        h2("Documentation"),
+        tags$body(
+            "Using the 'iris' data set, this web app allows you to explore the effect of varying folds and repeats when perfoming cross validation during model training. Three common predictive models are considered (see plot):"
+        ),
+        tags$li("linear discriminant analysis (lda)"),
+        tags$li("random forests (rf)"),
+        tags$li("gradient boosting machine (gbm)"),
+        tags$body(
+            "Simply adjust the sliders for the number of folds and repeats left to right and you can observe the impacts on the accuracy of the resulting models."
+        )
+    )
+))
